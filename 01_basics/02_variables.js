4b9f1e860156f654965eb22b240f8159f6465779
Pratibha <103841806+Pratibha0280@users.noreply.github.com>
2024-01-10 09:02:02 +0000

    renamed:    01_basics.js -> 01_basics/01_basics.js 	new file:   01_basics/02_variables.js 	new file:   01_basics/03_datatype.js

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,14 @@
+const rollNo = 12113000
+let name = "Pratibha Yadav"
+
+let state;
+//by default - undefined
+
+//avoid var as block scope  and functional scope issue
+var course = "Information"
+institute = "abc"
+
+name = "Prati"
+course = "IT"
+
+console.table([rollNo, name, course, institute,state])
---
+++ 01_basics/03_datatype.js
@@ -0,0 +1,16 @@
+"use strict"; //treat js code as newer version
+
+//alert("Hello") // can't use alert in nodejs
+
+console.log(2+3)
+console.log("hello");
+
+
+let name = "pratibha"
+let age = 20
+let student = true
+console.table([name, age, student])
+
+console.log(typeof student)
+console.log(typeof null)
+console.log(typeof undefined)
