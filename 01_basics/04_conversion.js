762463e6e930419e0e944f60d0c7a78aaf44bd66
Pratibha <103841806+Pratibha0280@users.noreply.github.com>
2024-01-11 16:24:39 +0000

    new file:   01_basics/04_conversion.js

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,40 @@
+let  score = "33"
+let newScore = Number(score)
+//console.log(typeof score)
+//console.log(typeof newScore)
+
+let score2 = true
+let newScore2 = Number(score2)
+//console.log(typeof score2)
+//console.log(typeof newScore2)
+//console.log(newScore2)
+
+
+let a = "pratibha"
+let b= Boolean(a)
+//console.log(typeof b)
+//console.log(b)
+
+
+/* 1- true    0-false
+  string non empty - true    empty-false
+*/
+
+
+
+let x = undefined
+let y = String(x)
+console.log(y)
+
+//operations
+
+console.log(1+2)
+console.log("1"+ "2")
+console.log(1 + 2 + "3")
+console.log("1"+ 2 + 3 + 4)
+
+console.log("second")
+console.log(true)
+console.log(+true)
+console.log(+"")
+//console.log(true+) //error here
