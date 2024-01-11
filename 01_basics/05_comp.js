51d801761f305bc958c3aa9a63dd83e7ad76d96c
Pratibha <103841806+Pratibha0280@users.noreply.github.com>
2024-01-11 16:34:44 +0000

    new file:   01_basics/05_comp.js

---
+++ 01_basics/05_comp.js
@@ -0,0 +1,18 @@
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//working of == is entriely differnet from greater than and less than
+
+
+
+//so unpredictable in case of different data types conversion
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined<0)
+
+
+
+
+//so try to avoid these type of conversions
+
